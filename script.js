d3b4d7294d5e242e0fbf4337d530101c9d2a02c8
Natali Kriginski Nunes da Rocha <110261630+NataliRocha@users.noreply.github.com>
2022-09-05 21:22:45 -0300

    up

--- script.js
+++ script.js
@@ -32,112 +32,184 @@ function verificar(){
 
 
 
+
+
+
+
+
 function contar(){
- var inicio = document.getElementById('txt1')
- var fim = document.getElementById('txt2')
 
-/*
- 
-if (inicio.value.length == 0||fim.value.length == 0){
-    alert('[ERRO: Faltam dados!')}
-
-else if (inicio.value > fim.value){
-    alert('Opção Inválida!')
-}   
-
-else if (inicio.value <1 || fim.value <1){
-    cont.innerHTML='Opção Inválida!'
-
-}
+    cont.innerHTML = ''
+    so.innerHTML = ''
+    var inicio = document.getElementById('txt1')
+    var fim = document.getElementById('txt2')
     
- else{*/
-
-//cont.innerHTML= 'Contando: '
-
-var i = Number(inicio.value)
-var f= Number(fim.value)
-var c = i
-
-while (c<=f) {
     
-    verifica2()
+    var f= Number(fim.value)
+    var resultado 
+    var i = Number(inicio.value)
+    
+    var resut =0
+    var soma4=0
+     var soma7=0
+    var resut2=0
 
-          // cont.innerHTML+=` ${c} `
-          
-          
-          
-        c++
-       
 
-       
+
+
+
+    if (inicio.value.length === 0){
+        alert('[ERRO]: Preencha um número!')
     }
+    else if (i> f){
+        alert('Opção Inválida!')}
 
-
-
-}
-
-
-function verifica2(c){
-    /*
-
-    if (c < 1 || c ==1){
-        cont.innerHTML= ``
         
-    } else if (c==2){
-        cont.innerHTML+= `${c} \u{1F449}`
-   //soma=c
    
+        
+  
+
+
+
+
+
+
+
+
+   
+        
+    else {
+        var i = Number(inicio.value)
+        var f= Number(fim.value)
+        var c =i
+       
+        verificarC()
+    function verificarC(){
+        var num2 = document.getElementById('txt1')
+        var primo = Number (num2.value) 
     
-    }
-       else  { 
-        
-      */ 
-        for (var n=2; n<c; n++){
-                resultado = c % n
-               
-               if (resultado==0){
-                cont.innerHTML+= ``
-                
-            return true
-            } 
-                cont.innerHTML+=` ${c}  \u{1F449}`
-                
-               /* soma= soma + c
-                soma1.innerHTML=`${soma}`*/
-                
-
-            return false  
-        
-
-                  
-                  
-                                } 
-             }
-             
+        if (num2.value.length == 0){
+            alert('[ERRO]: Preencha um número!')}
+            
+            else if (primo < 1 || primo ==1 ){
+                cont.innerText= ''}
+                else if (primo == 2){
+                    cont.innerText= ''}
+                    
+            
+           else  { 
             
            
+        for (var i=2; i<primo; i++){
+                resultado = primo % i;
+               
+               if (resultado==0){
+                cont.innerText= ''
+            return true
+            }  }
+            
             
 
+            
+              cont.innerText= + primo+ '\u{1F449}'
+              return false
+
+    
+            } }
         
 
     
+    while (c <= f){
+      
+
+if(c==2){
+    cont.innerText+=  +c+  '\u{1F449}'
+
+}
+
+
+
+var soma10=  document.getElementById("cont")
+var soma9= isNaN(soma10)
+var soma11 =0
+
+
+
+
+    c++
+   
+        if (c>f){
+            break
+        }
+        
+
+
+
+
+
+
+    if(c>2){
+var j =2
+while ( j<=c){
+
+    var resultado
+    
+    resultado = c % j
+    j++
+    if (resultado==0 ){
+        cont.innerText+= ''
+        break 
+
+}
+
+
+
+if (j==c){
+
+cont.innerText+= ` ${c}  \u{1F449}`
+
+if(i<=2){
+var soma=2 
+var soma1 =c
+var soma2 = soma+soma1
+var soma4=soma2+resut
+so.innerHTML= 'A soma destes números é:'+soma4+''
+ soma=0
+var resut=soma4-2
+
+}
+else{
+
+    if (soma10=NaN==true){
+        soma9=i
+    }
+
+var soma5 = soma9
+var soma6=c
+var soma7 = soma5+soma6
+var soma8= soma7+ resut2
+so.innerHTML= 'A soma destes números é: '+soma8+''
+resut2=soma8-soma5
+soma5=''
+
+
+
+}
+
+}
+
+
+}
 
  
 
-
-
-
-
-/*for (var c = i; c <=f; c+=i){
-    cont.innerHTML+= `${c} `*/
-
-
-
-
-
-
+} 
+ 
+ 
+    }
 
  
+}}
 
 
 
